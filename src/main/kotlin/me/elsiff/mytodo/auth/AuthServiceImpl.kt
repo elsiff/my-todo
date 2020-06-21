19648cf22894003704935a9f51e9dd33c2e054f8
elsiff <elsiff1002@gmail.com>
2020-06-22 01:32:36 +0900

    Add password encryption

--- src/main/kotlin/me/elsiff/mytodo/auth/AuthServiceImpl.kt
+++ src/main/kotlin/me/elsiff/mytodo/auth/AuthServiceImpl.kt
@@ -3,24 +3,28 @@ package me.elsiff.mytodo.auth
 import me.elsiff.mytodo.auth.jwt.JwtSigner
 import me.elsiff.mytodo.user.User
 import me.elsiff.mytodo.user.UserRepository
+import me.elsiff.mytodo.user.UserService
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Mono
 
 @Service
 class AuthServiceImpl(
         private val userRepository: UserRepository,
-        private val jwtSigner: JwtSigner
+        private val jwtSigner: JwtSigner,
+        private val passwordEncoder: BCryptPasswordEncoder,
+        private val userService: UserService
 ) : AuthService {
     override fun signIn(credentials: Mono<UserCredentials>): Mono<String> {
         return credentials
             .flatMap {
                 userRepository.findByUsername(it.username)
-                    .filter { user -> user.password == it.password }
+                    .filter { user -> passwordEncoder.matches(it.password, user.password) }
             }
             .map {
                 jwtSigner.createJwt(it.username)
             }
     }
 
-    override fun signUp(user: Mono<User>) = userRepository.create(user)
+    override fun signUp(user: Mono<User>) = userService.createUser(user)
 }
--- src/main/kotlin/me/elsiff/mytodo/user/UserServiceImpl.kt
+++ src/main/kotlin/me/elsiff/mytodo/user/UserServiceImpl.kt
@@ -1,5 +1,7 @@
 package me.elsiff.mytodo.user
 
+import org.springframework.context.annotation.Bean
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Mono
 import reactor.kotlin.core.publisher.toMono
@@ -9,16 +11,23 @@ import javax.annotation.PostConstruct
 class UserServiceImpl(
     private val userRepository: UserRepository
 ) : UserService {
+    @Bean
+    fun passwordEncoder() = BCryptPasswordEncoder()
+
     override fun getUser(username: String) = userRepository.findByUsername(username)
 
     override fun searchUsers() = userRepository.findAll()
 
     override fun createUser(user: Mono<User>) =
         userRepository.create(user.map {
+            it.password = passwordEncoder().encode(it.password)
             it
         })
 
-    override fun save(user: Mono<User>) = userRepository.save(user)
+    override fun save(user: Mono<User>) = userRepository.save(user.map {
+        it.password = passwordEncoder().encode(it.password)
+        it
+    })
 
     override fun deleteUser(username: String) =
         userRepository.deleteByUsername(username).map { it.deletedCount > 0 }
@@ -31,7 +40,7 @@ class UserServiceImpl(
 
     companion object {
         val INITIAL_USERS: List<User> = listOf(
-                User("admin", "admin", "admin@my-todo.elsiff.me")
+            User("admin", "admin", "admin@my-todo.elsiff.me")
         )
     }
 }
