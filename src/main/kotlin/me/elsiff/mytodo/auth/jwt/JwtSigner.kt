f8e69dcc43f7306fecfabcc754d30dc9a7204c22
elsiff <elsiff1002@gmail.com>
2020-06-22 00:52:18 +0900

    Change to sign jwt with the HS256 and the secret from application.yml

--- src/main/kotlin/me/elsiff/mytodo/auth/jwt/JwtSigner.kt
+++ src/main/kotlin/me/elsiff/mytodo/auth/jwt/JwtSigner.kt
@@ -5,22 +5,27 @@ import io.jsonwebtoken.Jws
 import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.SignatureAlgorithm
 import io.jsonwebtoken.security.Keys
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import java.security.KeyPair
 import java.time.Duration
 import java.time.Instant
 import java.util.*
+import javax.crypto.SecretKey
 
 /**
  * Created by elsiff on 2020-06-10.
  */
 @Service
-class JwtSigner {
-    val keyPair: KeyPair = Keys.keyPairFor(SignatureAlgorithm.RS256)
+class JwtSigner(
+    @Value("\${jwt.secret}")
+    val secret: String
+) {
+    val secretKey: SecretKey = Keys.hmacShaKeyFor(secret.toByteArray())
 
     fun createJwt(username: String): String {
         return Jwts.builder()
-            .signWith(keyPair.private, SignatureAlgorithm.RS256)
+            .signWith(secretKey, SignatureAlgorithm.HS256)
             .setSubject(username)
             .setIssuer("my-todo.elsiff.me")
             .setExpiration(Date.from(Instant.now().plus(Duration.ofMinutes(15))))
@@ -30,7 +35,7 @@ class JwtSigner {
 
     fun validateJwt(jwt: String): Jws<Claims> {
         return Jwts.parserBuilder()
-            .setSigningKey(keyPair.public)
+            .setSigningKey(secretKey)
             .build()
             .parseClaimsJws(jwt)
     }
